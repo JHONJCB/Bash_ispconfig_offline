ececfc33011324e1befe9776661f67a046151b53
JHONJCB <36390500+JHONJCB@users.noreply.github.com>
2025-11-06 22:20:43 -0500

    Update install_ispconfig_offline.sh

--- install_ispconfig_offline.sh
+++ install_ispconfig_offline.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-# ================================================================
-# Script: install_ispconfig_offline.sh
-# Descripción: Instala ISPConfig desde un paquete offline alojado en GitHub
-# Autor: ChatGPT
-# ================================================================
-# --- CONFIGURACIÓN ---
-WORKDIR="Bash_ispconfig_offline-main"
-echo "=============================================="
-echo "Instalación offline de ISPConfig iniciada..."
-echo "=============================================="
+# ===========================================================
+# Script: offline_prepare.sh
+# Propósito: preparar todos los paquetes necesarios para
+# instalar ISPConfig offline en Ubuntu Server 22.04 o 24.04
+# ===========================================================
 
-# --- CLONA EL REPOSITORIO OFFLINE ---
-echo "Descargando carpeta desde GitHub..."
+echo "Instalando unzip..."
+apt install unzip
+echo "======================================================="
+
+echo "Descargando el directorio de paquetes..."
+echo "Este proceso puede demorar un poco, espera..."
 wget https://github.com/JHONJCB/ispconfig_offline/archive/refs/heads/main.zip
+echo "======================================================="
 
-#Extraer el directorio
-unzip ispconfig_offline-main.zip
+echo "Descomprimiendo el archivo descargado..."
+unzip main.zip
+echo "======================================================="
+
+echo "Entrando al directorio descomprimido..."
+cd ispconfig_offline-main/ispconfig_offline
+echo "======================================================="
+
+echo "Ejecutando los paquetes e instalando dependencias..."
+sudo dpkg -i *.deb
+sudo apt --fix-broken install -y
+echo "======================================================="
+
+echo "Instalando ISPConfig..."
+cd ispconfig3-develop/install
+sudo php install.php
+echo "======================================================="
+
+echo "Intenta abrir ISPConfig ingresando a https://<IP_DEL_SERVIDOR>:8080"
+echo "Recuerda que la contraseña de acceso fue mostrada al finalizar la instalación"
